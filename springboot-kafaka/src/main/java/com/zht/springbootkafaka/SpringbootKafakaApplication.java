049db56d1996d6f608c9171ce64bfcd48c8d0354
lansebingchuan <1669638693@qq.com>
2020-08-02 12:56:06 +0800

    Shiro安全认证

---
+++ springboot-kafaka/src/main/java/com/zht/springbootkafaka/SpringbootKafakaApplication.java
@@ -0,0 +1,16 @@
+package com.zht.springbootkafaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZHT
+ */
+@SpringBootApplication
+public class SpringbootKafakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafakaApplication.class, args);
+    }
+
+}
---
+++ springboot-kafaka/src/main/java/com/zht/springbootkafaka/service/KafkaService.java
@@ -0,0 +1,13 @@
+package com.zht.springbootkafaka.service;
+
+/**
+ * <p>
+ * kafka服务实现类
+ * </p>
+ *
+ * @author ZHT
+ * @since 2020/7/30
+ */
+public interface KafkaService {
+
+}
