d4f04eebe278a5ea03e819808e531169da4dd6f5
austin.zhang <zkteco2020>
2021-12-04 14:46:43 +0800

    集成swagger2.9.2。并授权登录

--- springboot-oauth2/src/main/java/com/zht/config/security/WebSecurityConfig.java
+++ springboot-oauth2/src/main/java/com/zht/config/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,6 +34,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**").antMatchers("/webjars/**", "/swagger-resources/**", "/swagger**", "/v2/api-docs");
     }
 
     /**
